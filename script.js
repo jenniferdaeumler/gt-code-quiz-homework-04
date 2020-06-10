36491593e3ea8e1784cff0fd5a8d7f09c952e0d1
jdaeumler <jenniferdaeumler@gmail.com>
2020-06-10 16:01:38 -0500

    fixed colors. made beginButton seperate row. changed button id names to no numbers

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ var timerEl = document.querySelector(".time");
 //change timer seconds to 60 before end
 var secondsLeft = 5;
 //Buttons for answers
-var questionButtonOne = document.getElementById("1button");
-var questionButtonTwo = document.getElementById("2button");
-var questionButtonThree = document.getElementById("3button");
-var questionButtonFour = document.getElementById("4button");
+var questionButtonOne = document.getElementById("buttonOne");
+var questionButtonTwo = document.getElementById("buttonTwo");
+var questionButtonThree = document.getElementById("buttonThree");
+var questionButtonFour = document.getElementById("buttonFour");
 var questionText = document.getElementById("questionParagraph");
 
 var currentQuestionIndex = 0;
